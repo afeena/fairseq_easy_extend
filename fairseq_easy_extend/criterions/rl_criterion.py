c4e6b29bb9001c88470653f6dff905fd2e841fc9
Evgeniia <vremendm@gmail.com>
2023-05-10 13:33:17 +0200

    updates

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -1,8 +1,15 @@
+import math
+from argparse import Namespace
 
+import torch
 from fairseq.criterions import FairseqCriterion, register_criterion
+from fairseq.data import encoders
 from fairseq.dataclass import FairseqDataclass
 
 from dataclasses import dataclass, field
+
+from fairseq.logging import metrics
+
 
 @dataclass
 class RLCriterionConfig(FairseqDataclass):
@@ -15,6 +22,10 @@ class RLCriterion(FairseqCriterion):
     def __init__(self, task, sentence_level_metric):
         super().__init__(task)
         self.metric = sentence_level_metric
+        self.tokenizer = encoders.build_tokenizer(Namespace(
+            tokenizer='moses'
+        ))
+        self.tgt_dict = task.target_dictionary
 
     def forward(self, model, sample, reduce=True):
         """Compute the loss for the given sample.
@@ -37,7 +48,7 @@ class RLCriterion(FairseqCriterion):
         outs = outputs["word_ins"].get("out", None)
         masks = outputs["word_ins"].get("mask", None)
 
-        loss = self._compute_loss(outs, tgt_tokens, masks)
+        loss, reward = self._compute_loss(outs, tgt_tokens, masks)
 
         # NOTE:
         # we don't need to use sample_size as denominator for the gradient
@@ -49,10 +60,40 @@ class RLCriterion(FairseqCriterion):
             "ntokens": ntokens,
             "nsentences": nsentences,
             "sample_size": sample_size,
+            "reward": reward.detach()
         }
 
         return loss, sample_size, logging_output
 
+    def decode(self, toks, escape_unk=False):
+        with torch.no_grad():
+            s = self.tgt_dict.string(
+                toks.int().cpu(),
+                "@@ ",
+                # The default unknown string in fairseq is `<unk>`, but
+                # this is tokenized by sacrebleu as `< unk >`, inflating
+                # BLEU scores. Instead, we use a somewhat more verbose
+                # alternative that is unlikely to appear in the real
+                # reference, but doesn't get split into multiple tokens.
+                unk_string=(
+                    "UNKNOWNTOKENINREF" if escape_unk else "UNKNOWNTOKENINHYP"
+                ),
+            )
+            s = self.tokenizer.decode(s)
+        return s
+
+    def compute_reward(self, outputs, targets):
+        """
+        #we take a softmax over outputs
+        probs = F.softmax(outputs, dim=-1)
+        #argmax over the softmax \ sampling (e.g. multinomial)
+        samples_idx = torch.multinomial(probs, 1, replacement=True)
+        sample_strings = self.tgt_dict.string(samples_idx)  #see dictionary class of fairseq
+        #sample_strings = "I am a sentence"
+        reward_vals = evaluate(sample_strings, targets)
+        return reward_vals, samples_idx
+        """
+        pass
 
     def _compute_loss(self, outputs, targets, masks=None):
         """
@@ -115,4 +156,19 @@ class RLCriterion(FairseqCriterion):
         #For more about mask see notes on NLP2-notes-on-mask
 
 
-        return loss
+
+        return loss, reward.mean()
+
+    @staticmethod
+    def reduce_metrics(logging_outputs) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = sum(log.get("loss", 0) for log in logging_outputs)
+        reward_sum= sum(log.get("reward", 0) for log in logging_outputs)
+        ntokens = sum(log.get("ntokens", 0) for log in logging_outputs)
+        sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
+
+        # we divide by log(2) to convert the loss from base e to base 2
+        metrics.log_scalar(
+            "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
+        )
+        metrics.log_scalar("reward", reward_sum / sample_size, sample_size, round=3)
--- fairseq_easy_extend/dataclass/utils.py
+++ fairseq_easy_extend/dataclass/utils.py
@@ -5,6 +5,7 @@ import inspect
 import logging
 import os
 import re
+import sys
 from argparse import ArgumentError, ArgumentParser, Namespace
 from dataclasses import _MISSING_TYPE, MISSING, is_dataclass
 from enum import Enum
@@ -506,10 +507,15 @@ def merge_with_parent(dc: FairseqDataclass, cfg: DictConfig, remove_missing=Fals
 
 
 def _flatten_config(cfg, parent_key="", sep="_"):
+    if sys.version_info.major == 3 and sys.version_info.minor >= 10:
+
+        from collections.abc import MutableMapping as mm
+    else:
+        from collections import MutableMapping as mm
     items = []
     for k, v in cfg.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, mm):
             items.extend(_flatten_config(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))
