155539a6fa3f990aed1777dc66b9d2381a43f389
abhinav-neil <abhinav.neil@gmail.com>
2023-05-23 11:38:32 +0000

    train nat model & save checkpoint; test decode

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -1,32 +1,40 @@
 import math
 from argparse import Namespace
-
 import torch
+import torch.nn.functional as F
 from fairseq.criterions import FairseqCriterion, register_criterion
 from fairseq.data import encoders
 from fairseq.dataclass import FairseqDataclass
-
 from dataclasses import dataclass, field
-
 from fairseq.logging import metrics
-
+from sacrebleu.metrics import BLEU, CHRF
+import wandb
+wandb.init(project="nlp2-nanmt")
 
 @dataclass
 class RLCriterionConfig(FairseqDataclass):
-    sentence_level_metric: str = field(default="bleu",
-                                       metadata={"help": "sentence level metric"})
-
-
+    sentence_level_metric: str = field(default="bleu", metadata={"help": "sentence level metric"}) 
+    sampling: str = field(default="multinomial", metadata={"help": "sampling method"})
+    detokenization: bool = field(default=True,  metadata={"help": "whether to detokenize the output"})
+    use_wandb: bool = field(default=True, metadata={"help": "whether to use wandb"})
+    wandb_project: str = field(default="nlp2-nanmt", metadata={"help": "wandb project"})
+    
 @register_criterion("rl_loss", dataclass=RLCriterionConfig)
 class RLCriterion(FairseqCriterion):
-    def __init__(self, task, sentence_level_metric):
+    def __init__(self, task, sentence_level_metric, sampling="multinomial", wandb_project='nlp2-nanmt'):
         super().__init__(task)
         self.metric = sentence_level_metric
-        self.tokenizer = encoders.build_tokenizer(Namespace(
-            tokenizer='moses'
-        ))
+        self.tokenizer = encoders.build_tokenizer(Namespace(tokenizer='moses'))
         self.tgt_dict = task.target_dictionary
-
+        self.tgt_lang = "en"
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.sampling = sampling
+        self.bleu = BLEU(effective_order="True")
+        self.chrf = CHRF()
+        
+        # init wandb project
+        wandb.init(project=wandb_project)
+        
     def forward(self, model, sample, reduce=True):
         """Compute the loss for the given sample.
         Returns a tuple with three elements:
@@ -44,6 +52,7 @@ class RLCriterion(FairseqCriterion):
         tgt_tokens, prev_output_tokens = sample["target"], sample["prev_target"]
 
         outputs = model(src_tokens, src_lengths, prev_output_tokens, tgt_tokens)
+        
         #get loss only on tokens, not on lengths
         outs = outputs["word_ins"].get("out", None)
         masks = outputs["word_ins"].get("mask", None)
@@ -62,6 +71,9 @@ class RLCriterion(FairseqCriterion):
             "sample_size": sample_size,
             "reward": reward.detach()
         }
+        
+        # if reduce:
+            # self.reduce_metrics(logging_output)
 
         return loss, sample_size, logging_output
 
@@ -101,7 +113,55 @@ class RLCriterion(FairseqCriterion):
         targets: batch x len
         masks:   batch x len
         """
+        # input to device
+        outputs = outputs.to(self.device)
+        targets = targets.to(self.device)
+        
+        bsz, seq_len, vocab_size = outputs.size()
 
+        # sample predictions and convert predictions and targets to strings without tokenization and bpe
+        probs = F.softmax(outputs, dim=-1)
+        with torch.no_grad():
+            # multinomial or argmax sampling
+            if self.sampling == "multinomial":
+                preds = torch.multinomial(probs.view(-1, vocab_size), 1, replacement=True).view(bsz, seq_len)
+            elif self.sampling == "argmax":
+                preds = torch.argmax(probs.view(-1, vocab_size), dim=-1).view(bsz, seq_len)
+            
+            # detokenization
+            preds_str = [self.decode(pred) for pred in preds]
+            targets_str = [self.decode(target) for target in targets]
+            # print(f'1st target sent: {targets_str[0]}\n1st pred sent: {preds_str[0]}')
+            
+            # compute reward metric
+            if self.metric == "bleu":
+                reward = torch.tensor([[self.bleu.sentence_score(pred, [targ]).score] * seq_len for pred, targ in zip(preds_str, targets_str)])
+            elif self.metric == "chrf":
+                reward = torch.tensor([[self.chrf.sentence_score(pred, [targ]).score] * seq_len for pred, targ in zip(preds_str, targets_str)])
+            else:
+                raise ValueError(f"metric {self.metric} not supported")
+        # print(f'reward: {reward}')
+        reward = torch.tensor(reward).to(self.device)
+        # print(f'shape of probs: {probs.shape}, shape of targets: {targets.shape}, shape of masks: {masks.shape}, shape of preds: {preds.shape}, shape of rewards: {reward.shape}')
+        # apply mask
+        if masks is not None:
+            masks = masks.to(self.device)
+            probs, targets = probs[masks], targets[masks]
+            # outputs, targets = outputs[masks], targets[masks]
+            reward, preds = reward[masks], preds[masks]
+
+        # get the log probs of the samples
+        # log_probs = F.log_softmax(probs, dim=-1)
+        log_probs = torch.log(probs)
+        sample_log_probs = torch.gather(log_probs, -1, preds.unsqueeze(1)).squeeze()
+        
+        # calculate loss of all samples and average for batch loss
+        loss = -sample_log_probs * reward
+        loss, reward = loss.mean(), reward.mean()
+        print(f'loss: {loss.item():.3f} | reward: {reward:.3f}')    
+        
+        return loss, reward
+        
         #padding mask
         ##If you take mask before you do sampling: you sample over a BATCH and your reward is on token level
         #if you take mask after, you sample SENTENCES and calculate reward on a sentence level 
@@ -155,10 +215,6 @@ class RLCriterion(FairseqCriterion):
         
         #For more about mask see notes on NLP2-notes-on-mask
 
-
-
-        return loss, reward.mean()
-
     @staticmethod
     def reduce_metrics(logging_outputs) -> None:
         """Aggregate logging outputs from data parallel training."""
@@ -172,3 +228,9 @@ class RLCriterion(FairseqCriterion):
             "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
         )
         metrics.log_scalar("reward", reward_sum / sample_size, sample_size, round=3)
+        
+        # log to wandb
+        wandb.log({
+            "loss": loss_sum / sample_size / math.log(2),
+            "reward": reward_sum / sample_size,
+        })
